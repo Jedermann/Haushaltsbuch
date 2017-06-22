5bc8f7916bda212612fe8ba12b2e9217baaedb94
Jedermann <Manu.schmidt99@gmail.com>
2017-06-22 23:27:14 +0200

    Main hinzufügen

---
+++ Haushaltsbuch/src/ext/Main.java
@@ -0,0 +1,13 @@
+package ext;
+
+import view.Eintrag_Tabelle;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Eintrag_Tabelle xtb = new Eintrag_Tabelle();
+		xtb.setVisible(true);		
+	}
+
+}
