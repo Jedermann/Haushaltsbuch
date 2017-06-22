be6d10feb13429afa14cfc14cb2ffa295babebb2
Martin Eller <eller.martin29@gmail.com>
2017-06-23 00:25:04 +0200

    Javadoc hinzugefügt

--- Haushaltsbuch/src/controller/EndeListener.java
+++ Haushaltsbuch/src/controller/EndeListener.java
@@ -11,11 +11,17 @@ import java.util.*;
 public class EndeListener implements ActionListener {
 	
 	private Eintrag_Tabelle gui;
-	
+	/**
+	 * 
+	 * @param gui
+	 */
 	public EndeListener(Eintrag_Tabelle gui) {
 		this.gui = gui;
 	}
-
+	/**
+	 * Action performed method for closing the Gui
+	 * @param ActionEvent arg0
+	 */
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		System.exit(0);
--- Haushaltsbuch/src/ext/Main.java
+++ Haushaltsbuch/src/ext/Main.java
@@ -3,7 +3,10 @@ package ext;
 import view.Eintrag_Tabelle;
 
 public class Main {
-
+	/**
+	 * Main method
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Eintrag_Tabelle xtb = new Eintrag_Tabelle();
